867ab0dcac8a37bce43020e840e240c1c858218e
Kiz <kieran.mcdougall@aamcommercial.com.au>
2019-07-07 12:41:42 +1000

    Fits within

--- extract/positioned_node.py
+++ extract/positioned_node.py
@@ -2,5 +2,17 @@ class PositionedNode(object):
     def __init__(self, bbox):
         self.bbox = bbox
 
-    def fits_in(self):
+    def fits_in(self, bbox):
+        if self.bbox.upper_left_coordinate.x < bbox.upper_left_coordinate.x:
+            return False
+
+        if self.bbox.upper_left_coordinate.y < bbox.upper_left_coordinate.y:
+            return False
+
+        if self.bbox.lower_right_coordinate.x > bbox.lower_right_coordinate.x:
+            return False
+
+        if self.bbox.lower_right_coordinate.y > bbox.lower_right_coordinate.y:
+            return False
+
         return True
