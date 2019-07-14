2c3663692ccd6262102cd92b6740cc65e88857c4
Kiz <kieran.mcdougall@aamcommercial.com.au>
2019-07-14 15:18:07 +1000

    Min and max x boundary on page

--- extract/page.py
+++ extract/page.py
@@ -10,6 +10,24 @@ class Page(PositionedNode):
         super().__init__(bbox_from_node_attrs(attr))
         self.text_boxes = []
 
+    def min_x_boundary(self):
+        xs = []
+        for text_box in self.text_boxes:
+            for text_line in text_box:
+                for text in text_line:
+                    xs.append(text.bbox.upper_left_coordinate.x)
+
+        return min(xs)
+
+    def max_x_boundary(self):
+        xs = []
+        for text_box in self.text_boxes:
+            for text_line in text_box:
+                for text in text_line:
+                    xs.append(text.bbox.lower_right_coordinate.x)
+
+        return max(xs)
+
     def dump(self) -> str:
         c = ''
         for text_box in self.text_boxes:
